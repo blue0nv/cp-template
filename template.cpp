204630cf08ef58fdfc47ee38df6be3363cbf4e75
Bahaa Ghonim <bahaamaher996@gmail.com>
2026-04-18 15:55:01 +0200

    Refactor sieve function

--- template.cpp
+++ template.cpp
@@ -7,33 +7,31 @@ using namespace std;
 #define allr(x) x.rbegin(), x.rend()
 #define gottaGoFast() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-const int N = 1e6 + 5;
+const int N = 2000000;
 
 bool primes[N];
-void sieve(int n)
+void sieve()
 {
-    primes[0] = false;
-    for (int i = 1; i <= n; i++) primes[i] = true;
-
-    for (int i = 2; i <= n; i++)
+    for (int i = 1; i < N; i++) primes[i] = true;
+    primes[0] = primes[1] = false;
+    for (int i = 2; i * i <= N; i++)
     {
         if (primes[i])
         {
-            for (int j = i * i; j <= n; j += i) primes[j] = false;
+            for (int j = i * i; j <= N; j += i) primes[j] = false;
         }
     }
 }
 
-void Bluify()
+void solve()
 {
-    
 }
 
 signed main()
 {
     gottaGoFast();
     int t = 1;
-    // cin >> t;
-    while (t--) Bluify();
+   // cin >> t;
+    while (t--) solve();
     return 0;
 }
